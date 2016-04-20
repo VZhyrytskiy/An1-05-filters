a0b4f54390048131c6a9fb8cf093f604f7a659e7
Vitaliy Zhyrytskyy <vitaliy.zhyrytskyy@epam.com>
2016-04-20 09:37:15 +0300

    Start Point

---
+++ app/app.js
@@ -0,0 +1,38 @@
+(function() {
+	'use strict';
+
+	angular
+		.module("app", ["ngSanitize", "feature"])
+		.controller("defaultController", defaultController);
+
+	function defaultController(productData) {
+		var vm = this;
+		vm.products = productData.products;
+
+
+		// vm.htmlSnippet = 
+		// 	'Pretty text with some links:\n'+
+		// 	'http://angularjs.org/,\n'+
+		// 	'mailto:us@somewhere.org,\n'+
+		// 	'another@somewhere.org,\n'+
+		// 	'and one more: ftp://127.0.0.1/.';
+	}
+
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ app/feature/productDataSrv.js
@@ -0,0 +1,25 @@
+(function() {
+	"use strict";
+
+	angular
+		.module("feature")
+		.factory("productData", productData);
+
+	function productData() {
+		return {
+			products: [
+				{"name": "Apples", "category": "Fruit", "price": 1.20, "expiry": 10},
+				{"name": "Bananas", "category": "Fruit", "price": 2.42, "expiry": 7},
+				{"name": "Pears", "category": "Fruit", "price": 2.02, "expiry": 6},
+				{"name": "Tuna", "category": "Fish", "price": 20.45, "expiry": 3},
+				{"name": "Salmon", "category": "Fish", "price": 17.93, "expiry": 2},
+				{"name": "Trout", "category": "Fish", "price": 12.93, "expiry": 4},
+				{"name": "Beer", "category": "Drinks", "price": 2.99, "expiry": 365},
+				{"name": "Wine", "category": "Drinks", "price": 8.99, "expiry": 365},
+				{"name": "Whisky", "category": "Drinks", "price": 45.99, "expiry": 365}
+			]
+		};
+	};
+
+})();
+
